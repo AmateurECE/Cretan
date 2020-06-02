9b9882860c3121ed0b951691ecc1ca876b2abe31
AmateurECE <edtwardy@mtu.edu>
2020-06-01 20:50:29 -0400

    Add packaging script
    This patch adds a packaging script which can be used to upload the
    package to the pypi server.

---
+++ package.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+###############################################################################
+# NAME:             package.sh
+#
+# AUTHOR:           Ethan D. Twardy <edtwardy@mtu.edu>
+#
+# DESCRIPTION:      This shell script generates the package files for
+#                   distribution
+#
+# CREATED:          06/01/2020
+#
+# LAST EDITED:      06/01/2020
+###
+
+python3 setup.py sdist
+
+cat - > index.html <<EOF
+<!doctype html>
+<html>
+  <body>
+EOF
+
+for dir in "cretan.egg-info dist"; do
+    for f in `find $dir -type f`; do
+        printf '    <a href="%s">%s</a>\n' "$f" "$f" >> index.html
+    done
+done
+
+cat - >> index.html <<EOF
+  </body>
+</html>
+EOF
+
+rsync -rvz --progress -e 'ssh -p 5000' $DIRECTORIES index.html \
+      edtwardy@edtwardy.hopto.org:/var/www/edtwardy.hopto.org/pypi/cretan/
+
+###############################################################################
